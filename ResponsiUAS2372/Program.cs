92146ca3fb581edbd689be5a9348dc0f91cdca88
Vickasunflora <vsunflora@gmail.com>
2019-07-23 11:29:37 +0700

    Add project files.

---
+++ ResponsiUAS2372/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiUAS2372
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
